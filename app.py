eae476bb3e8d468beeff02c73fe13b2a2dac003c
Joseph Francis <josephfrancis100@gmail.com>
2019-09-14 10:11:13 -0500

    Layout

--- app.py
+++ app.py
@@ -18,15 +18,16 @@ posts = [
 
 ]
 
+
 @app.route('/')
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts, title="Home")
 
 
 @app.route("/about")
 def about():
-    return "About page"
+    return render_template("about.html", title="About")
 
 
 if __name__ == '__main__':
