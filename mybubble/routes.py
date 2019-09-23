8facde5d794329ddf402e245e7cd8f915899d6e7
Joseph Francis <josephfrancis100@gmail.com>
2019-09-22 23:09:33 -0500

    Login/Logout

--- mybubble/routes.py
+++ mybubble/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect, url_for
 from mybubble import app, db, bcrypt
 from mybubble.models import User
 from mybubble.registration_form import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -33,6 +34,9 @@ def about():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
@@ -46,14 +50,30 @@ def register():
     return render_template("register.html", title="Register", form=form)
 
 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "josephfrancis100@gmail.com" and form.password.data == "dec131997":
-            flash("You have logged in!", "success")
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for("home"))
         else:
             flash("Login unsuccessful", "danger")
 
     return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/account")
+@login_required
+def account(): 
+    return render_template("account.html", title="Account", name="Joseph")
